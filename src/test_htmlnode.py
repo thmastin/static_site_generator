94ca6f25c0c733594b61159daf65f345e8f811a1
thmastin <thmastin@gmail.com>
2024-08-25 03:08:56 -0400

    Fix expected output in THMLNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -12,7 +12,15 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_out(self):
         node = HTMLNode("h1", "This is test text", ["h2"], {"href": "<a>"})
         node_text = node.props_to_html()
-        test_text = f" href <a>"
+        print(node_text)
+        test_text = f' href="<a>"'
+        self.assertEqual(node_text, test_text)
+    
+    def test_multiple_props(self):
+        node = HTMLNode("h1", "This is test text", ["h2"], {"href": "<a>", "<h1>": "heading"})
+        node_text = node.props_to_html()
+        print(node_text)
+        test_text = f' href="<a>" <h1>="heading"'
         self.assertEqual(node_text, test_text)
 
 
