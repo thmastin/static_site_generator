7a7c81c7f789c580c74404fbb3e777ef49218a71
thmastin <thmastin@gmail.com>
2024-08-25 01:04:50 -0400

    Update TextNode and create test

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import TextNode
+
+def main():
+    test_text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    
+    print(test_text_node)
+
+
+main()    
