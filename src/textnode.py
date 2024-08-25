97a0a72cb65f1ddc9f477eadc1c53e128f548c4e
thmastin <thmastin@gmail.com>
2024-08-25 00:45:20 -0400

    Add new TextNode class with init method

---
+++ src/textnode.py
@@ -0,0 +1,6 @@
+class TextNode(self, text, text_type, url):
+    def __init__(self,text,tex_type,url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+        
