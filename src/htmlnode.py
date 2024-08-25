9464ed6fe06c2c84b94e3d44e8ca4cc74ce3241c
thmastin <thmastin@gmail.com>
2024-08-25 01:51:24 -0400

    Add HTMLNoce class

---
+++ src/htmlnode.py
@@ -0,0 +1,27 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("Subclasses should overwrite this")
+
+    def props_to_html(self):
+        output = ""
+        if self.props == None:
+            raise Exception("No Properties")
+        for key in self.props:
+            output = output + " " + key + " " + self.props[key]
+        return output
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+
+    
+
+
+
+        
